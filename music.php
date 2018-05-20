281f54aabf64e9342bc52777b5aa91ff8b59fefe
RavshanovUsmonbek <36451884+RavshanovUsmonbek@users.noreply.github.com>
2018-05-20 13:21:18 +0500

    Update music.php

--- music.php
+++ music.php
@@ -7,8 +7,33 @@
 		<link href="webpage/viewer.css" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
-		<div id="header">
+		<?php 
+		function size_rounder($size)
+		{
+			$stsize;
+			if($size>0 && $size<1024)
+			{
+				$stsize=strval($size);
+				return $stsize."b";
+			}
+			elseif ($size>=1024 && $size<1048576) {
 
+				$stsize=strval(round($size/1024,2));
+				return $stsize."kb";
+			}
+			elseif ($size>=1048576 ) {
+				$stsize=strval(round($size/1048576,2));
+				return $stsize."mb";
+			}
+		}
+		?>
+		
+			<?php if(isset($_REQUEST['playlist'])){ ?>
+				<a href="music.php" style="border: 1px solid black; padding:5px ; border-radius: 2px; background-color: lightblue;color:red; "> BACK </a>
+				<?php } ?>
+
+
+		<div id="header">
 			<h1>190M Music Playlist Viewer</h1>
 			<h2>Search Through Your Playlists and Music</h2>
 		</div>
@@ -17,6 +42,7 @@
 		<div id="listarea">
 			<ul id="musiclist">
 				<?php
+				$size;
 				if(!isset($_REQUEST["playlist"]))
 			 	{
 			 		$songs=glob("webpage/songs/*.mp3");
@@ -25,15 +51,15 @@
 				else {
 					$playlist="webpage/songs/".urldecode($_REQUEST["playlist"]);
 					$songs=file($playlist);
+					$files=NULL;
 					for ($i=0; $i < count($songs); $i++) { 
 						$songs[$i]="webpage/songs/".$songs[$i];
 					}
-					$files=NULL;
 				}
-				foreach ( $songs as $song) {
+				for ($i=0; $i < count($songs); $i++) {
 				?>
 				<li class="mp3item">
-					<a href= "<?= $song ?>"><?=basename($song)?></a>
+					<a href= "<?= $songs[$i] ?>"><?=basename($songs[$i])?></a><?php if($files){echo "("; echo size_rounder(filesize($songs[$i])); echo ")";} ?>
 				</li>
 				<?php } 
 				if(isset($files)){
